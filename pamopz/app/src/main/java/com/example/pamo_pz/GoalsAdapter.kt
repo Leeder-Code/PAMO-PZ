9f439b94262dcbcffc46cdac4c1e0a665601eaeb
Mati <mati.leeder@gmail.com>
2024-07-03 19:45:03 +0200

    feat: create GoalsAdapter

---
+++ pamopz/app/src/main/java/com/example/pamo_pz/GoalsAdapter.kt
@@ -0,0 +1,70 @@
+package com.example.pamo_pz
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pamo_pz.databinding.ItemSavingsGoalBinding
+
+/**
+ * Adapter for displaying savings goals in a RecyclerView.
+ *
+ * @property goals List of goals to display.
+ */
+class GoalsAdapter(private var goals: List<Goal>) :
+    RecyclerView.Adapter<GoalsAdapter.GoalViewHolder>() {
+    /**
+     * ViewHolder for displaying individual goals.
+     *
+     * @param binding View binding for the item layout.
+     */
+    inner class GoalViewHolder(private val binding: ItemSavingsGoalBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        /**
+         * Binds a goal to the ViewHolder.
+         *
+         * @param goal The goal to bind.
+         */
+        @SuppressLint("SetTextI18n")
+        fun bind(goal: Goal) {
+            binding.textViewGoalName.text = goal.name
+            binding.textViewGoalDescription.text = goal.description
+            binding.textViewAmountSaved.text = "$${goal.value} / $${goal.target}"
+            binding.progressBarGoal.setProgress(((goal.value / goal.target) * 100).toInt())
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GoalViewHolder {
+        val binding =
+            ItemSavingsGoalBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return GoalViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: GoalViewHolder, position: Int) {
+        holder.bind(goals[position])
+    }
+
+    override fun getItemCount(): Int {
+        return goals.size
+    }
+
+    /**
+     * Updates the list of goals and notifies the adapter of the change.
+     *
+     * @param newGoals The new list of goals to update.
+     */
+    fun updateGoals(newGoals: List<Goal>) {
+        goals = newGoals
+        notifyDataSetChanged()
+    }
+
+    /**
+     * Sets the data for the adapter with a new list of goals and notifies the adapter of the change.
+     *
+     * @param goals The list of goals to set.
+     */
+    fun setData(goals: List<Goal>) {
+        this.goals = goals
+        notifyDataSetChanged()
+    }
+}
